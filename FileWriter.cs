354813bc0ac5fd1f5881ea8526a784710e35f8b7
Miguel Usma <usmamiguel04@gmail.com>
2024-04-13 00:31:27 -0500

    FileWriter Class

---
+++ FileWriter.cs
@@ -0,0 +1,15 @@
+using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.ValueObjects;
+
+namespace NorthWind.Writers
+{
+    public class FileWriter : IUserActionWriter
+    {
+        public void Write(UserAction action)
+        {
+            File.AppendAllText("AppLogs.txt",
+                string.Format("ConsoleWriter: {0}, {1}, {2}",
+            action.CreatedDateTime, action.User, action.Description));
+        }
+    }
+}
